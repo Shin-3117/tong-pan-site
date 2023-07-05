0245b19ee8a7b30ce21763e632bf74f84966a3df
Shin-3117 <tlswnd3127@gmail.com>
2023-07-05 15:22:54 +0900

    add ssafy

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
 import styles from "./page.module.css"
+import Image from "next/image"
+import inputPng from '/public/insert-picture-icon.png'
+import earth from '/public/earth.png'
+import icons from '/public/icons.png'
+import megaphone2 from '/public/megaphone2.png'
+import network from '/public/network.png'
+import worldline from '/public/world-line.png'
 
 export default function Home() {
   return (
     <>
     <main className={styles.imgContainer}>
-      <div className={styles.imgColum}>사진들1</div>
-      <div className={styles.imgColum}>사진들2</div>
+      <div className={styles.imgColum}>
+        <Image src={inputPng} className={styles.img}/>
+        <Image src={icons} className={styles.img}/>
+        <Image src={network} className={styles.img}/>
+      </div>
+      <div className={styles.imgColum}>
+        <Image src={earth} className={styles.img}/>
+        <Image src={megaphone2} className={styles.img}/>
+        <Image src={worldline} className={styles.img}/>
+      </div>
     </main>
     </>
   )
